78acb5b7c21d769254f9b50f249bd0df25db0046
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-22 23:25:18 +0200

    improve cli for fcfs

--- src/FCFSScheduling.java
+++ src/FCFSScheduling.java
@@ -1,27 +1,26 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class Process {
-    String name;
-    int arrivalTime;
-    int burstTime;
-    int waitingTime;
-    int turnaroundTime;
-
-    public Process(String name, int arrivalTime, int burstTime) {
-        this.name = name;
-        this.arrivalTime = arrivalTime;
-        this.burstTime = burstTime;
-        this.waitingTime = 0;
-        this.turnaroundTime = 0;
-    }
-}
-
 class FCFSScheduling {
+    // ANSI escape codes for text colors
+    private static final String RESET = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String CYAN = "\u001B[36m";
+    private static final String BOLD = "\u001B[1m";
+
     public static void run(ArrayList<Process> processes) {
         int currentTime = 0;
         int totalWaitingTime = 0;
         int totalTurnaroundTime = 0;
+
+        System.out.println("\n" + BOLD + "Process Execution Table" + RESET);
+        System.out.println("+-----------+--------------+-------------+-----------------+");
+
+        System.out.printf("| %-9s | %-12s | %-11s | %-15s |\n", "Process", "Arrival Time", "Burst Time", "Completion Time");
+        System.out.println("+-----------+--------------+-------------+-----------------+");
 
         for (Process process : processes) {
             if (process.arrivalTime > currentTime) {
@@ -31,42 +30,62 @@ class FCFSScheduling {
             process.waitingTime = currentTime - process.arrivalTime;
             totalWaitingTime += process.waitingTime;
 
-            System.out.println("Executing " + process.name + " at time " + currentTime);
+            System.out.printf("| %-9s | %-12d | %-11d | %-15d |\n", process.name, process.arrivalTime, process.burstTime, currentTime + process.burstTime);
             currentTime += process.burstTime;
 
             process.turnaroundTime = process.waitingTime + process.burstTime;
             totalTurnaroundTime += process.turnaroundTime;
-
-            System.out.println("Process " + process.name + " - Waiting Time: " + process.waitingTime +
-                    ", Turnaround Time: " + process.turnaroundTime);
         }
+
+        System.out.println("+-----------+--------------+-------------+-----------------+");
 
         double averageWaitingTime = (double) totalWaitingTime / processes.size();
         double averageTurnaroundTime = (double) totalTurnaroundTime / processes.size();
 
-        System.out.println("Average Waiting Time: " + averageWaitingTime);
-        System.out.println("Average Turnaround Time: " + averageTurnaroundTime);
+        System.out.println("\n" + BOLD + "Summary" + RESET);
+        System.out.println("Average Waiting Time: " + CYAN + averageWaitingTime + RESET);
+        System.out.println("Average Turnaround Time: " + CYAN + averageTurnaroundTime + RESET);
     }
 
     public static void runWithUserInput() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of processes: ");
-        int numProcesses = scanner.nextInt();
+        try {
+            System.out.print("Enter the number of processes: ");
+            int numProcesses = scanner.nextInt();
 
-        ArrayList<Process> processes = new ArrayList<>();
-        for (int i = 1; i <= numProcesses; i++) {
-            System.out.println("Enter details for process P" + i + ":");
-            System.out.print("Arrival Time: ");
-            int arrivalTime = scanner.nextInt();
-            System.out.print("Burst Time: ");
-            int burstTime = scanner.nextInt();
+            if (numProcesses <= 0) {
+                System.out.println(RED + "Number of processes must be greater than 0." + RESET);
+                return;
+            }
 
-            processes.add(new Process("P" + i, arrivalTime, burstTime));
+            ArrayList<Process> processes = new ArrayList<>();
+            for (int i = 1; i <= numProcesses; i++) {
+                System.out.println("Enter details for process P" + i + ":");
+
+                int arrivalTime = getValidInput(scanner, "Arrival Time: ");
+                int burstTime = getValidInput(scanner, "Burst Time: ");
+
+                processes.add(new Process("P" + i, arrivalTime, burstTime));
+            }
+
+            run(processes);
+        } catch (InputMismatchException e) {
+            System.out.println(RED + "Invalid input. Please enter a valid integer." + RESET);
+        } finally {
+            scanner.close();
         }
+    }
 
-        scanner.close();
-
-        run(processes);
+    private static int getValidInput(Scanner scanner, String prompt) {
+        while (true) {
+            try {
+                System.out.print(prompt);
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println(RED + "Invalid input. Please enter a valid integer." + RESET);
+                scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
+            }
+        }
     }
 }
