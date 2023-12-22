13b8737f033db08ecd133122b74f8f53f4a554cc
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-23 00:50:33 +0200

    change main functionality

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-    // ANSI escape codes for text colors
-    public static final String RESET = "\u001B[0m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String BOLD = "\u001B[1m";
-
+    // ANSI escape codes for colors
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("\n" + CYAN + "🌟 Welcome to CPU Scheduling Simulator! 🌟" + RESET);
+        Scanner scanner = new Scanner(System.in);
+        // Print header with colors
+        System.out.println(ANSI_BLUE + "Choose a scheduling algorithm:" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "----------------------------------" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "Algorithm" + "\t\t" + "Description" + ANSI_RESET);
+        System.out.println(ANSI_GREEN + "----------------------------------" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "1." + "\t\t\t" + "FCFS (First Come First Serve)" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "2." + "\t\t\t" + "SJF (Shortest Job First - Non-Preemptive)" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "3." + "\t\t\t" + "SJF (Shortest Job First - Preemptive)" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "4." + "\t\t\t" + "Round Robin" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "5." + "\t\t\t" + "Priority Scheduling (Preemptive)" + ANSI_RESET);
+        System.out.println(" " + ANSI_YELLOW + "6." + "\t\t\t" + "Priority Scheduling (Non-Preemptive)" + ANSI_RESET);
 
-            while (true) {
-                displayMenu();
+        System.out.print("Enter the number of the selected algorithm: ");
+        int selectedAlgorithm = scanner.nextInt();
 
-                System.out.print("\nEnter the number of the selected algorithm (Enter 0 to exit): ");
-                int selectedAlgorithm = getIntInput(scanner);
-
-                System.out.println(); // Add an empty line for better separation
-
-                if (selectedAlgorithm == 0) {
-                    System.out.println(GREEN + "👋 Thank you for using the CPU Scheduling Simulator. Goodbye!" + RESET);
-                    break;
-                }
-
-                switch (selectedAlgorithm) {
-                    case 1:
-                        runAlgorithm("FCFS (First Come First Serve)", YELLOW, FCFSScheduling::runWithUserInput);
-                        break;
-                    case 2:
-                        runAlgorithm("SJF (Shortest Job First - Non-Preemptive)", YELLOW,
-                                SJFNonPreemptiveScheduling::runWithUserInput);
-                        break;
-                    case 3:
-                        runAlgorithm("SJF (Shortest Job First - Preemptive)", YELLOW,
-                                SJFPreemptiveScheduling::runWithUserInput);
-                        break;
-                    case 4:
-                        runAlgorithm("Round Robin", YELLOW, () -> RoundRobin.main(args));
-                        break;
-                    case 5:
-                        runAlgorithm("Priority Scheduling (Preemptive)", YELLOW,
-                                () -> PriorityPreemptiveScheduling.main(args));
-                        break;
-                    case 6:
-                        runAlgorithm("Priority Scheduling (Non-Preemptive)", YELLOW,
-                                () -> NonPreemptivePriorityCPUSchedulingAlgorithm.main(args));
-                        break;
-                    default:
-                        System.out.println(RED + "❌ Invalid choice. Please choose a valid algorithm." + RESET);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(RED + "💥 An error occurred: " + e.getMessage() + RESET);
+        switch (selectedAlgorithm) {
+            case 1:
+                FCFSScheduling.runWithUserInput();
+                break;
+            case 4:
+                RoundRobin.main(args);
+                break;
+            case 5:
+                PriorityPreemptiveScheduling.main(args);
+                break;
+            case 6:
+                NonPreemptivePriorityCPUSchedulingAlgorithm.main(args);
+                break;
+            case 2:
+                SJFNonPreemptiveScheduling.runWithUserInput();
+                break;
+            case 3:
+                SJFPreemptiveScheduling.runWithUserInput();
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose a valid algorithm.");
         }
-    }
 
-    private static void displayMenu() {
-        System.out.println("Choose a CPU scheduling algorithm:");
-        System.out.println("1. " + GREEN + "FCFS (First Come First Serve)" + RESET);
-        System.out.println("2. " + GREEN + "SJF (Shortest Job First - Non-Preemptive)" + RESET);
-        System.out.println("3. " + GREEN + "SJF (Shortest Job First - Preemptive)" + RESET);
-        System.out.println("4. " + GREEN + "Round Robin" + RESET);
-        System.out.println("5. " + GREEN + "Priority Scheduling (Preemptive)" + RESET);
-        System.out.println("6. " + GREEN + "Priority Scheduling (Non-Preemptive)" + RESET);
-        System.out.println("0. " + GREEN + "Exit" + RESET);
-    }
-
-    private static void runAlgorithm(String algorithmName, String color, Runnable algorithmRunner) {
-        System.out.println(color + "Running " + BOLD + algorithmName + BOLD + " algorithm..." + RESET);
-        algorithmRunner.run();
-    }
-
-    private static int getIntInput(Scanner scanner) {
-        while (true) {
-            try {
-                return Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.print(RED + "❌ Invalid input. Please enter a number: " + RESET);
-            }
-        }
+        scanner.close();
     }
 }
